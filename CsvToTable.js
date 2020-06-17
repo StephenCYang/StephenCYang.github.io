198a168913d7082f1cfef18e6dd88e2e9651cdd5
Stephen Yang <26841782+StephenCYang@users.noreply.github.com>
2020-06-17 15:11:46 -0700

    Update CsvToTable.js

--- CsvToTable.js
+++ CsvToTable.js
@@ -65,7 +65,7 @@
 	function buildTable() {
 		getCSV.call(this).then(function(response){
 			var allRows = response.split(/\r?\n|\r/).filter(isNotEmpty);
-	        var table = '<table class="table table-striped table-nonfluid">';
+	        var table = '<table class="table table-striped table-nonfluid" style="width:80%">';
 	        for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
 	            if (singleRow === 0) {
 	                table += '<thead>';
